f89e08e3cf79ffed7c01e2e3a3756787b2fa7d78
Ricardo <Ricardo@10.1.48.126>
2015-11-07 09:05:42 -0200

    Primeira alteração! =)

--- FreteWeb/src/main/java/br/senai/freteweb/DAO/MotoristaDAO.java
+++ FreteWeb/src/main/java/br/senai/freteweb/DAO/MotoristaDAO.java
@@ -5,18 +5,44 @@
  */
 package br.senai.freteweb.DAO;
 
+import br.senai.freteweb.model.Motorista;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 /**
  *
  * @author Ricardo
  */
+
 @Stateless 
 public class MotoristaDAO {
     
     @PersistenceContext(unitName = "freteWebPU")
     private EntityManager em;
     
+    public void insere(Motorista motorita){
+        em.persist(motorita);
+    }
+    
+    public void excluir(Long id){
+        em.remove(em.getReference(Motorista.class, id));
+    }
+    
+    public Motorista buscar(Long id){
+        return em.find(Motorista.class, id);
+    }
+    
+    public Motorista atualizar(Motorista motorista){
+        return em.merge(motorista);
+    }
+    
+    public List<Motorista> listar(){
+        TypedQuery<Motorista> q = em.createQuery("Select m"
+            + "From Motorista m Order by m.nome", Motorista.class);
+                    
+        return q.getResultList();
+    }
 }
