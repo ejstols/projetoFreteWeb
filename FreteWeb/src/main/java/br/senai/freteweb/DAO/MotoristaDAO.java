1fca8ae8f03c2f7eea12934bdd41df1311b66e63
Ricardo <Ricardo@10.1.48.126>
2015-11-06 22:11:46 -0200

    Primeira alteração! =)

---
+++ FreteWeb/src/main/java/br/senai/freteweb/DAO/MotoristaDAO.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senai.freteweb.DAO;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Ricardo
+ */
+//@Stateless 
+public class MotoristaDAO {
+    
+  //  @PersistenceContext(unitName = "freteWebPU")
+  //  private EntityManager em;
+    
+}
